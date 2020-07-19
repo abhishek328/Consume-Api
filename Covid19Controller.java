b5000dd463e62b060a664c2d1c4ef1d057eb129c
Abhishek <babhishek328@gmail.com>
2020-07-19 19:19:18 +0530

    api class created

---
+++ Covid19Controller.java
@@ -0,0 +1,25 @@
+package com.covid19;
+
+import com.covid19.Covid19;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class Covid19Controller {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/datacovid19")
+    public Covid19 getList()
+    {
+        String url = "https://run.mocky.io/v3/74f3dc86-3d2f-4dec-811a-3d68ef7d5679";
+        Covid19 object = restTemplate.getForObject(url,Covid19.class);
+        //System.out.println(object);
+        return object;
+
+    }
+
+}
---
+++ DailyData.java
@@ -0,0 +1,94 @@
+package com.covid19;
+
+public class DailyData {
+
+    private String dailyconfirmed ;
+    private String dailydeceased ;
+    private String dailyrecovered ;
+    private String date ;
+    private String totalconfirmed;
+    private String totaldeceased ;
+    private String totalrecovered ;
+
+    public DailyData() {
+    }
+
+    public DailyData(String dailyconfirmed, String dailydeceased, String dailyrecovered, String date, String totalconfirmed, String totaldeceased, String totalrecovered) {
+        this.dailyconfirmed = dailyconfirmed;
+        this.dailydeceased = dailydeceased;
+        this.dailyrecovered = dailyrecovered;
+        this.date = date;
+        this.totalconfirmed = totalconfirmed;
+        this.totaldeceased = totaldeceased;
+        this.totalrecovered = totalrecovered;
+    }
+
+    public String getDailyconfirmed() {
+        return dailyconfirmed;
+    }
+
+    public void setDailyconfirmed(String dailyconfirmed) {
+        this.dailyconfirmed = dailyconfirmed;
+    }
+
+    public String getDailydeceased() {
+        return dailydeceased;
+    }
+
+    public void setDailydeceased(String dailydeceased) {
+        this.dailydeceased = dailydeceased;
+    }
+
+    public String getDailyrecovered() {
+        return dailyrecovered;
+    }
+
+    public void setDailyrecovered(String dailyrecovered) {
+        this.dailyrecovered = dailyrecovered;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTotalconfirmed() {
+        return totalconfirmed;
+    }
+
+    public void setTotalconfirmed(String totalconfirmed) {
+        this.totalconfirmed = totalconfirmed;
+    }
+
+    public String getTotaldeceased() {
+        return totaldeceased;
+    }
+
+    public void setTotaldeceased(String totaldeceased) {
+        this.totaldeceased = totaldeceased;
+    }
+
+    public String getTotalrecovered() {
+        return totalrecovered;
+    }
+
+    public void setTotalrecovered(String totalrecovered) {
+        this.totalrecovered = totalrecovered;
+    }
+
+    @Override
+    public String toString() {
+        return "DailyData{" +
+                "dailyconfirmed='" + dailyconfirmed + '\'' +
+                ", dailydeceased='" + dailydeceased + '\'' +
+                ", dailyrecovered='" + dailyrecovered + '\'' +
+                ", date='" + date + '\'' +
+                ", totalconfirmed='" + totalconfirmed + '\'' +
+                ", totaldeceased='" + totaldeceased + '\'' +
+                ", totalrecovered='" + totalrecovered + '\'' +
+                '}';
+    }
+}
